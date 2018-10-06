b30d4b02845219c11b22ee905c70ef72d65a7373
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-07 00:39:40 +0530

    Show Files in directory without linq

--- Program.cs
+++ Program.cs
@@ -11,7 +11,20 @@ namespace Introduction
     {
         static void Main(string[] args)
         {
-            
+            string path = @"C:\Windows";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
+
         }
     }
 }
