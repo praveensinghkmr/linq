e0950618942816629fd6c079e8c9c1de6f051e95
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-20 20:39:20 +0530

    Querying the infinite collection

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -8,6 +8,12 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+
+            var numbers = MyLinq.Random().Where(n => n > 0.5);//.Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
             var movies = new List<Movie>
             {
                 new Movie { Title = "The Dark Knight", Rating = 8.9f, Year = 2008 },
