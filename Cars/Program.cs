bdb0355e4234050b5d21d496bc330c115b5e0e64
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-21 13:06:05 +0530

    Reading data from csv file. Using ext. method format

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace Cars
     {
         static void Main(string[] args)
         {
+            var cars = ProcessFile("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return 
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(line => line.Length > 1)
+                    .Select(Car.ParseFromCsv)
+                    .ToList();
         }
     }
 }
